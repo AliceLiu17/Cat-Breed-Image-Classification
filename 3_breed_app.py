d4f5389efeabb0787c313b08bd0676f03b026622
Alice Liu <87574148+AliceLiu17@users.noreply.github.com>
2022-12-03 22:16:46 -0500

    3 and 2 breed app. Working. Need fix result

---
+++ 3_breed_app.py
@@ -0,0 +1,62 @@
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+from PIL import Image
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+#helper function
+def get_image(upload_file):
+    st.image(Image.open(upload_file))
+    img_array = np.array(Image.open(upload_file).convert('L').resize((300, 256))).reshape(1, 300*256)
+    return img_array
+
+def get_image_type(model, uploaded_file):
+    img_array = get_image(uploaded_file)
+    return model.predict(img_array)[0]
+
+
+# app:
+st.set_page_config(
+    page_title= "Cat Breed Image Classification Demo",
+    page_icon= "random",
+    menu_items= {
+        "Get Help": "https://github.com/AliceLiu17/Cat-Breed-Image-Classification",
+        "Report a bug": "https://github.com/AliceLiu17/Cat-Breed-Image-Classification/issues/new",
+        "About": """ **APPLICATION:** Provides cat breed prediction based on image classification"""
+    }
+)
+
+c1, c2, c3 = st.columns([1, 6, 1])
+with c2:
+    st.title("Cat Breed Image Classifier")
+    upload_file = st.file_uploader("")
+
+    if upload_file is not None:
+        # ML model
+        # svm_model = pickle.load(open("svm_model.pkl", "rb"))
+        svm_model = pickle.load(open("3_breed_model.pkl", "rb"))
+
+        # results
+        image_type = get_image_type(svm_model, upload_file)
+
+        # st.header(image_type)
+        
+        if image_type == 2 :
+            st.header(f"The cat is a bombay")
+        elif image_type == 1:
+            st.header(f"The cat is a siamese")
+        elif image_type == 0:
+            st.header(f"The cat is a tabby")
+        else:
+            st.header(f"Sorry, our model isn't trained on that cat!")
+
+    else:
+        st.info(f"""Upload a .JPG file please!""")
+        st.stop()
+
+        
+
